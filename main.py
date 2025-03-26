d2aa3557cf348428fd4ab536b8331085360e79cf
Billy Clay <BillyClay.2010@gmail.com>
2025-03-26 12:13:12 -0500

    added player.py and circleshape.py, updated main and constants

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import *
 
 from constants import *
 
@@ -11,6 +12,8 @@ def main():
 	clock= pygame.time.Clock()
 	dt = 0
 	keep_playing = True
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
@@ -18,7 +21,11 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		screen.fill(000)
+		screen.fill(0)
+		player.draw(screen)
+
+
+
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 		#dt = clock.get_time() / 1000
