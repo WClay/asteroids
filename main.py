13368ddd9916ad13a45c640d954986b6ec647c12
Billy Clay <BillyClay.2010@gmail.com>
2025-03-27 13:52:51 -0500

    switched to using groups in main.py

--- main.py
+++ main.py
@@ -9,28 +9,35 @@ from constants import *
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	clock= pygame.time.Clock()
-	dt = 0
-	keep_playing = True
+	clock = pygame.time.Clock()
+	updatable = pygame.sprite.Group() 
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+	keep_playing = True
+	dt = 0
+	
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+
+
 	while keep_playing:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		screen.fill(0)
-		player.draw(screen)
+			
+		screen.fill("black")
 
+		updatable.update(dt)
 
-
+		for thing in drawable:
+			thing.draw(screen)
+		
 		pygame.display.flip()
-		dt = clock.tick(60) / 1000
-		#dt = clock.get_time() / 1000
 
-	
+		# limit the framerate to 60 FPS
+		dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
